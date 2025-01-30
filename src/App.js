015f9976d593e611c6bf776c10636bb3d0460e97
nguyenminhtien98 <nguyenminhtienhy98@gmail.com>
2025-01-31 00:40:31 +0700

    add react-query

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { publicRoutes } from '~/routes';
 import { DefaultLayout } from './Layouts';
-import { Fragment } from 'react';
+import { Fragment, useEffect } from 'react';
+import axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
 
 function App() {
+    // useEffect(() => {
+    //     fetchApi();
+    // }, []);
+    const fetchApi = async () => {
+        const res = await axios.get(`${process.env.REACT_APP_API_URL}/product/getAll`);
+        return res.data;
+    };
+    const query = useQuery({ queryKey: ['todos'], queryFn: fetchApi });
+    console.log('query', query);
+
     return (
         <Router>
             <div className="App">
--- src/index.js
+++ src/index.js
@@ -2,22 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 
 import { store } from './redux/store';
 import { Provider } from 'react-redux/es/exports';
 
 import GlobalStyles from './components/GlobalStyles';
 import './components/GlobalStyles/GridSystem.scss';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const queryClient = new QueryClient();
 root.render(
-    <React.StrictMode>
+    // <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
         <Provider store={store}>
             <GlobalStyles>
                 <App />
             </GlobalStyles>
         </Provider>
-    </React.StrictMode>,
+        <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>,
+    // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
