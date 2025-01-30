0910ee846287412c94abf5b3559246c14e3c294f
nguyenminhtien98 <nguyenminhtienhy98@gmail.com>
2025-01-31 01:59:47 +0700

    update

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
         return res.data;
     };
     const query = useQuery({ queryKey: ['todos'], queryFn: fetchApi });
-    console.log('query', query);
 
     return (
         <Router>
