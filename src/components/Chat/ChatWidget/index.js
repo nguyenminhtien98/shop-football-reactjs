b9ab24f05e1f6fb0e21240c6449ad42d69543f02
nguyenminhtien98 <nguyenminhtienhy98@gmail.com>
2025-04-24 18:10:17 +0700

    fix bug socket ChatWidget

--- src/components/Chat/ChatWidget/index.js
+++ src/components/Chat/ChatWidget/index.js
@@ -19,7 +19,7 @@ function ChatWidget() {
     useEffect(() => {
         const chatId = localStorage.getItem("chatId");
         if (!chatId) return;
-        socketRef.current = io('http://localhost:3001');
+        socketRef.current = io(process.env.REACT_APP_API_URL);
         socketRef.current.emit('joinRoom', chatId);
         socketRef.current.on('receive-message', (msg) => {
             if (!isOpen && msg.sender === "admin") {
